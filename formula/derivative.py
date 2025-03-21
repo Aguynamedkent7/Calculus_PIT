8db081a77110025af1dee3ee24cec9ab45bfa1d7
seodowa <pagalan.theodoreustp@gmail.com>
2025-03-22 00:40:44 +0800

    added basic dervative functions and tests

--- formula/derivative.py
+++ formula/derivative.py
@@ -1,6 +1,23 @@
 import sympy as smp
+import numpy as np
 
-x, y, z = smp.symbols('x y z', real=True)
 
-f = smp.diff(x**2 + 2*x + 1, x)
-print(f)
+def main():
+    pass
+
+
+def derivative_of_range(user_input, symbol_wrt, a, b, order=1):
+    fn = smp.parse_expr(user_input, {f'{symbol_wrt}': symbol_wrt})
+    f_prime = smp.diff(fn, symbol_wrt, order)
+    lambda_f_prime = smp.lambdify(symbol_wrt, f_prime, modules=['numpy'])
+    return lambda_f_prime(np.array([i for i in range(a, b)]))
+
+
+def derivative(user_input, symbol_wrt, order=1):
+    fn = smp.parse_expr(user_input, {f'{symbol_wrt}': symbol_wrt})
+    f_prime = smp.diff(fn, symbol_wrt, order)
+    return f_prime
+
+
+if __name__ == '__main__':
+    main()
